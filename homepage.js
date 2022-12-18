6417f1d0d7fd4d5a50610ca952b0e5bd037255f4
TechisHeaven <vermajihimanshu2004@gmail.com>
2022-12-18 15:17:16 +0530

    new js and css commit

--- homepage.js
+++ homepage.js
@@ -19,3 +19,47 @@ toggle.onclick = function(){
     side_bar[0].classList.toggle('dark');
     sidebaritems[0].classList.toggle('active');
 }
+
+toggle2.onclick = function(){
+    toggle2.classList.toggle('active');
+    body.classList.toggle('active');
+    containers[0].classList.toggle('active');
+    containers[1].classList.toggle('active');
+    containers[2].classList.toggle('active');
+    heading[0].classList.toggle('active');
+    heading[1].classList.toggle('active');
+    subnav_bar.classList.toggle('active')
+    searchInput[0].classList.toggle('active');
+    side_bar[0].classList.toggle('dark');
+    sidebaritems[0].classList.toggle('active');
+    profile_con.classList.toggle('active')
+}
+
+
+profilebtn.onclick = function(){
+    profile_con.classList.toggle("show")
+}
+
+
+
+
+
+
+const logosmall = document.getElementById("logo-small");
+const hamburger  = document.getElementById("hamburgur");
+const close = document.getElementById("close");
+
+logosmall.onclick = function(){
+    hamburger.classList.add('show');
+    hamburger.classList.add('showslide');
+    close.classList.remove("hidden");
+}
+close.onclick = function(){
+    hamburger.classList.add('showslide');
+    hamburger.classList.remove('show');
+    close.classList.add("hidden");
+
+}
+
+
+
